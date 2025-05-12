b162ab76a47dda4c6550759ca87d79592dd693f9
Fawad Nassery <eaa23fana@students.eaaa.dk>
2025-05-12 13:53:27 +0200

    Initial Commit

---
+++ BidServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BidServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
